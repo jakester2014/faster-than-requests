2a0475a914de9686db8677ed7027a3b95d02050b
Sourcery AI <>
2023-03-11 12:24:20 +0000

    'Refactored by Sourcery'

--- examples/example2.py
+++ examples/example2.py
@@ -4,12 +4,12 @@ import faster_than_requests as reqs
 
 
 time1 = time.time()
-for i in range(9):
+for _ in range(9):
   requests.get("https://httpbin.org/get").content
 print(f'Requests = {time.time() - time1}')
 
 time2 = time.time()
-for i in range(9):
+for _ in range(9):
   reqs.get2str("https://httpbin.org/get")
 print(f'Faster_than_requests = {time.time() - time2}')
 
